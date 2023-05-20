9fcd7e30efb1153a7c6dc191de83b647d472e1ec
Zainal Arifin <128154324+elzains@users.noreply.github.com>
2023-05-21 04:00:02 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var typed = new Typed('#typing', {
+    strings: ['<b>Delicious Meals for Campus Life.</b>', '<b>Savor the Awesomeness, Fuel Your Day!.</b>', '<b>Culinary Excellence, Where Flavor Meets Style!</b>'],
+    typeSpeed: 100,
+    loop: true,
+  });
